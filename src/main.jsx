687b1838df8618ba52d1b88fd8010c4927dce0fb
Anthony Nguyen <anthonynguye56@gmail.com>
2023-12-04 21:47:27 -0800

    imported pages from components, created browser router

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,40 @@
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App';
+import AboutPage from '/src/components/pages/AboutPage.jsx';
+import PortfolioPage from '/src/components/pages/PortfolioPage.jsx';
+import ResumePage from '/src/components/pages/ResumePage.jsx';
+import ContactPage from '/src/components/pages/ContactPage.jsx'
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <AboutPage />,
+      },
+      {
+        path: 'portfolio',
+        element: <PortfolioPage />,
+      },
+      {
+        path: 'resume',
+        element: <ResumePage />,
+      },
+      {
+        path: 'contact',
+        element: <ContactPage />,
+      },
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <App />
+  <RouterProvider router={router} />
 )
