34811e9904c7468fe31cdad93b520717f852ed7e
Anthony Nguyen <anthonynguye56@gmail.com>
2023-12-05 02:19:01 -0800

    changed head tags and removed some br tags

--- src/components/pages/ResumePage.jsx
+++ src/components/pages/ResumePage.jsx
@@ -4,11 +4,9 @@ const styles = {
         flexDirection: 'column',
         alignItems: 'center',
         flexWrap: 'wrap',
-        justifyContent: 'center',
         background: '#9a74db',
         gap: '2% 8%',
         position: 'relative',
-        bottom: 0,
         width: '100%',
         height: '100%',
         listStyleType: 'none',
@@ -20,27 +18,26 @@ const styles = {
 function Resume() {
     return (
         <div style={styles.container}>
-            <h1>Resume</h1>
-            <h2>Front-end Proficiencies:</h2>
-            <br/>
-            <li>HTML,</li>
-            <li>CSS,</li>
-            <li>JaveScript,</li>
-            <li>jQuery,</li>
-            <li>Responsive design,</li>
-            <li>React,</li>
-            <li>BootStrap,</li>
+            <h3>Resume</h3>
+            <h4>Front-end Proficiencies:</h4>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JaveScript</li>
+            <li>jQuery</li>
+            <li>Responsive design</li>
+            <li>React</li>
+            <li>BootStrap</li>
 
-            <br/>
-            <br/>
+            <br />
 
-            <h2>Back-end Proficiencies:</h2>
-            <br/>
-            <li>APIs,</li>
-            <li>Node,</li>
-            <li>Express,</li>
-            <li>MySQL, Sequelize</li>
-            <li>MongoDB, Mongoose</li>
+            <h4>Back-end Proficiencies:</h4>
+            <li>APIs</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>MySQL</li>
+            <li>Sequelize</li>
+            <li>MongoDB</li>
+            <li> Mongoose</li>
             <li>Rest</li>
             <li>GraphQL</li>
         </div>
